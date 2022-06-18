002ac360cc66253b7fd070103c40ea2e08bb3592
Abhishek Das <abhid14@outlook.com>
2022-06-19 02:38:58 +0530

    hmmm

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 // framework7
 var $$ = Dom7;
 app = new Framework7({
+  id: "RepairFlix",
   root: "#app",
   theme: "md",
   routes,
 });
+// firebase init
+const firebaseConfig = {
+  apiKey: "AIzaSyAqh32TWjbBZUNVAUxW_vwTsJMsWxNq5os",
+  authDomain: "repairflixapi.firebaseapp.com",
+  projectId: "repairflixapi",
+  storageBucket: "repairflixapi.appspot.com",
+  messagingSenderId: "595252406764",
+  appId: "1:595252406764:web:71c2361b7e1e56135ef8c9"
+};
+firebase.initializeApp(firebaseConfig);
+let isAuthenticated = false;
+setTimeout(function () {
+  if (!isAuthenticated) {
+    document.getElementById("intro").click();
+  }
+}, 300);
+function getNext(n) {
+  globalThis.swiper = document.querySelector(".swiper-container").swiper;
+  if (n > 0) {
+      globalThis.swiper = document.querySelector(".swiper-login").swiper;
+      swiper.slideTo(n);
+  } else {
+      swiper.slideNext();
+  }
+}
--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,10 @@
 var routes = [
-  { path: "/", url: "./index.html"},
+  { path: "/", url: "./index.html", name: "home" },
   { path: "/index.html", url: "./index.html" },
+  { path: "/intro/", url: "./pages/intro.html", name: "intro" },
+  { path: "/user-login/", url: "./pages/user-login.html", name: "user-login" },
+  { path: "/dept-login/", url: "./pages/centre-login.html", name: "centre-login" },
+  { path: "/user/", url: "./pages/user.html", name: "user" },
+  { path: "/dept/", url: "./pages/centre.html", name: "centre" },
+  { path: "(.*)", url: "./pages/404.html" },
 ];
